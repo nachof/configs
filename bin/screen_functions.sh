95c0eb6e2b735f84208d8231b6b6964a4fbdebca
Nacho <nacho@nucleartesuji.com>
2009-04-07 14:51:12 -0300

    Scripts for opening screen sessions

---
+++ bin/screen_functions.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+screen_available_sessions() {
+  screen -ls | grep '[0-9]\+\.\w' | cut -d. -f2 | cut -f1 | grep -v '^pts-' | sort
+}
+
+dialog() {
+  if [[ -n "$DISPLAY" ]]; then
+    echo "Xdialog"
+  else
+    echo "dialog"
+  fi
+}
+
+screen_open() {
+  echo '#!/bin/bash' > /tmp/thiscommand
+  echo '[[ -n "'$2'" ]] && echo -e "\033]2;'$2'\007"' >> /tmp/thiscommand
+  echo "screen $1 $2" >> /tmp/thiscommand
+  chmod u+x /tmp/thiscommand
+  if [[ -n "$DISPLAY" ]]; then
+    urxvt -e /tmp/thiscommand
+  else
+    /tmp/thiscommand
+  fi
+}
+
+screen_load_or_create() {
+  name=$1
+  if screen_available_sessions | grep -q "^$1$"; then
+    screen_open -x $1
+  else
+    screen_open -S $1
+  fi
+}
+
